f9b6c8ed7e37cc0cc47809dc0934811c5224ed15
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2022-10-13 21:40:43 +0100

    New Editor with experimental dialog boxes

--- PPIintT/PPIintT/editor.pl
+++ PPIintT/PPIintT/editor.pl
@@ -6,80 +6,39 @@ use lib "../lib";
 use open ":std", ":encoding(UTF-8)";
 use Term::Graille  qw/colour paint printAt cursorAt clearScreen border blockBlit block2braille pixelAt/;
 use Term::Graille::Interact ;
-use Term::Graille::Menu qw/deathSentence wrapText/;
+use Term::Graille::Menu;
 use Term::Graille::Textarea;
 use Term::Graille::Selector;
+use Term::Graille::Dialog;
 use Time::HiRes qw/sleep/;
 use Data::Dumper;
 
-my $canvas;
-
 my $io=Term::Graille::Interact->new();
 $io->{debugCursor}=[22,45];
-my $width  = $io->{terminalWidth}*1.8,
-my $height = $io->{terminalHeight}*2.3,
-my $mode="Editor";
-
-sub makeCanvas{
-	$canvas = Term::Graille->new(
-		width=>$width,
-		height=>$height,
-		top=>4,
-		left=>6,
-		borderStyle => "double",
-		title=>"Logo $mode",
-	  );
-}
 
 my @colours=qw/red blue green yellow cyan magenta white/;
 
 # setup keyboard short cuts
 
-my $menu=new Term::Graille::Menu(
+my $menu=new Term::Graille::Menu(  # no object offered so id will be o0 
           menu=>[["File","New","Load","Save","Quit"],
                  ["Edit",["Select","All","Start","End"],["Selection","Copy","Cut","Paste"],"Search","Replace"],
-                 ["View","Editor","Viewer"],
                  ["test","Death","Message","Input","Selector"],
                  "About"],
           redraw=>\&refreshScreen,
-          callback=>\&menuActions,
+          dispatcher=>\&menuActions,
           );
 
-my $textarea=new Term::Graille::Textarea();
-
-makeCanvas();
-refreshScreen();
-
+# in this case the dispatcher directs to subroutines based on name of  
 my %actions = ( New  => sub{},
                 Load => sub{ load()},
                 Save => sub{ save() },
                 Editor=>sub{mode("Editor")},
                 Viewer=>sub{mode("Viewer")},
+                Message=>sub{message("Test Message")},
               );
-          
-$io->addObject(object => $menu,trigger=>"m");
-$io->addObject(object => $textarea,trigger=>"e");
-
-$io->run();
-
-sub mode{
-	$mode=shift;
-	$canvas->{title}="Logo $mode";
-	if ($mode eq "Editor"){
-		$textarea->draw();
-	}
-	else{
-		$canvas->draw();
-	}
-}
-
-sub refreshScreen{
-	clearScreen() unless (shift);
-	$canvas->draw();
-	$textarea->draw();
-}
-
-sub menuActions{
+              
+sub menuActions{ # dispatcher for menu
 	my ($action,$gV)=@_;
 	if (exists $actions{$action}){
 		$actions{$action}->($gV)
@@ -89,10 +48,24 @@ sub menuActions{
 	}
 };
 
+my $textarea=new Term::Graille::Textarea();   # no object offered so id will be o1 
+
+refreshScreen();
+          
+$io->addObject(object => $menu,trigger=>"m");
+$io->addObject(object => $textarea,trigger=>"e");
+$io->run();
+$io->start("o1");
+
+sub refreshScreen{
+	clearScreen() unless (shift);
+	$textarea->draw();
+}
+
 sub load{
 	my ($dir,$filters)=@_;
 	$dir//=".";
-	$filters//="pl";
+	$filters//="\.txt|\.logo\$";
 	opendir(my $DIR, $dir) || die "Can't open directory $dir: $!";
 	my @files = sort(grep {(/$filters/) && -f "$dir/$_" } readdir($DIR));
 	closedir $DIR;
@@ -101,6 +74,7 @@ sub load{
           callback=>\&loadFile,
           options=>[@files],
           transient=>1,
+          title=>"Load File",
           );
     my $selId=$io->addObject(object => $selector);
     $io->start($selId);
@@ -109,6 +83,63 @@ sub load{
 
 sub loadFile{
 	my $file=shift;
+	open(my $fh, "<", "$file") or die("Can't open file $file:$! ");
+	chomp(my @lines = <$fh>);
+	close($fh);
+	$textarea->{text}=[@lines];
 	$io->close();
-	printAt(2,60,$file);
+	printAt(2,60,"$file loaded");
+	$io->start("o1");
+}
+
+sub save{
+	my ($dir,$filters)=@_;
+	$dir//=".";
+	$filters//=".txt\.logo\$";
+	opendir(my $DIR, $dir) || die "Can't open directory $dir: $!";
+	my @files = sort(grep {(/$filters/) && -f "$dir/$_" } readdir($DIR));
+	closedir $DIR;
+	my $selector=new Term::Graille::Selector(
+          redraw=>\&refreshScreen,
+          callback=>\&saveFile,
+          options=>[@files],
+          transient=>1,
+          title=>"Save File",
+          );
+    my $selId=$io->addObject(object => $selector);
+    $io->start($selId);
+	
+}		
+
+sub saveFile{
+	my $file=shift;
+	open(my $fh, ">", "$file") or die("Can't open file $file:$! ");
+	print $fh join("\n",@{$textarea->{text}});
+	close($fh);
+	$io->close();
+	printAt(2,60,"$file saved");
+	$io->start("o1");
+}
+
+sub message{
+	my ($message,$actionMatrix,$input)=@_;
+	my $dialog=new Term::Graille::Dialog(
+          redraw=>\&refreshScreen,
+          callback=>\&+
+          ,
+          message=>$message,
+          icon=>"info",
+          title=>"Test Message",
+          );
+    $dialog->mode("input");
+    my $dialogId=$io->addObject(object => $dialog);
+    $io->start($dialogId);
+}
+
+sub messageReturn{
+	my ($ret,@others)=@_;
+	$ret||="nothing" ;
+	$io->close();
+	if ($ret){ printAt(2,50,"$ret @others returned") }
+	
 }
